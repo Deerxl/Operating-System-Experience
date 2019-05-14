50e6b79dbdbcbf54fc26e5b677940c53fae859ef
xl Deer <YAOXL819@outlook.com>
2019-05-14 22:21:51 +0800

    2019.5.14

---
+++ OsExp/Project2/test.cpp
@@ -0,0 +1,214 @@
+/*
+可变分区管理方式下采用首次适应算法实现主存分配和回收
+[提示]：
+(1)	可变分区方式是按作业需要的主存空间大小来分割分区的。当要装入一个作业时，根据作业需要的主存容量查看是否有足够的空闲空间，若有，则按需分配，否则，作业无法装入。假定内存大小为128K，空闲区说明表格式为：
+·分区号——表示是第几个空闲分区；
+·起始地址——指出空闲区的起始地址；
+·长度——一个连续空闲区的长度；
+(2)	采用首次适应算法分配回收内存空间。运行时，输入一系列分配请求和回收请求。
+要求能接受来自键盘的空间申请及释放请求，能显示分区分配及回收后的内存布局情况。
+*/
+
+#include<iostream>
+using namespace std;
+
+#define MAXSIZE 128
+#define UNASSIGNED 0
+#define ASSIGNED 1
+
+struct Partition
+{
+	int num = NULL;           //分区号
+	int startAddr;     //起始地址
+	int length;		   //分区长度
+	int state;		   //状态         0 未分配 1 已分配
+	int tasknum = NULL;       //作业名
+	struct Partition* next;
+};
+
+bool Insert(Partition*& head, Partition*& newTast);
+bool Recycle(Partition*& head, Partition*& deleteTask);
+
+void Show(Partition*& head)
+{
+	Partition* pHead = head;
+	Partition* p = new Partition;
+	cout << "************************空闲分区表****************************" << endl;
+	cout << "分区号\t" << "起始地址\t" << "长度\t" << endl;	
+	
+	
+	while (pHead->next != NULL)
+	{
+		p = pHead->next;
+		if (p->state == UNASSIGNED)
+		{
+			cout << p->num << "\t" << p->startAddr << "\t\t" << p->length << endl;
+		}
+		pHead = pHead->next;
+	}
+	cout << "************************空闲分区表****************************" << endl;
+}
+
+void NewTask(Partition*& head)
+{
+	Partition* newTask = new Partition;
+	cout << "请为申请空间的任务设置 任务名（int） 和 空间大小（int）：" << endl;
+
+	cin >> newTask->tasknum >> newTask->length;
+	if (newTask->tasknum < 0 || newTask->length < 0)
+	{
+		cout << "请按照要求输入数据!" << endl;
+		exit(0);
+	}
+	if (Insert(head, newTask))
+	{
+		cout << "申请成功！" << endl;
+	}
+	else
+	{
+		cout << "申请失败！" << endl;
+	}
+
+}
+
+bool Insert(Partition*& head, Partition*& newTask)
+{
+	Partition* phead = head;
+	Partition* p = new Partition;
+	p = phead->next;
+	while (p != NULL)
+	{
+		if (p->length > newTask->length && (p->state == UNASSIGNED))
+		{
+			newTask->next = p;
+			phead->next = newTask;
+
+			newTask->startAddr = p->startAddr;
+			newTask->state = ASSIGNED;
+
+			p->startAddr += newTask->length;
+			p->length -= newTask->length;
+
+			return true;
+		}
+		else if (p->length == newTask->length && (p->state == UNASSIGNED))
+		{
+			p->state = ASSIGNED;
+			p->tasknum = newTask->tasknum;
+
+			while (phead->next->next != NULL)
+			{
+				phead = phead->next;
+				p = head->next;
+				if (p->state == UNASSIGNED)
+				{
+					p->num -= 1;
+				}
+				
+			}
+
+			return true;
+		}
+		phead = phead->next;
+		p = phead->next;
+	}
+
+	return false;
+}
+
+void DeleteTask(Partition*& head)
+{
+	Partition* deleteTask = new Partition;
+	cout << "请输入要释放空间的任务名：" << endl;
+
+	cin >> deleteTask->tasknum;
+	if (deleteTask->tasknum < 0)
+	{
+		cout << "请按照要求输入数据!" << endl;
+		exit(0);
+	}
+	if (Recycle(head, deleteTask))
+	{
+		cout << "释放成功！" << endl;
+	}
+	else
+	{
+		cout << "释放失败！" << endl;
+	}
+}
+
+bool Recycle(Partition*& head, Partition*& deleteTask)
+{
+	Partition* phead = head;
+	Partition* p = new Partition;
+	p = phead->next;
+	Partition* pre = NULL;
+	while (p != NULL)
+	{
+		if (p->tasknum == deleteTask->tasknum && p->state == ASSIGNED)
+		{
+			if (pre == NULL)
+			{
+				p->num = 1;
+			}
+			else
+			{
+				p->num = pre->num + 1;
+			}
+
+			p->state = UNASSIGNED;
+			while (phead->next->next != NULL)
+			{
+				phead = phead->next;
+				p = phead->next;
+				if (p->state == UNASSIGNED)
+				{
+					p->num += 1;
+				}
+				
+			}
+			return true;
+		}
+		if (p->state == UNASSIGNED)
+		{
+			pre = p;
+		}		
+		phead = phead->next;
+		p = phead->next;
+	}
+	return false;
+}
+
+int main()
+{
+	Partition* head = new Partition;
+	Partition* p = new Partition;
+	p->num = 1;
+	p->length = MAXSIZE;
+	p->startAddr = 0;
+	p->state = UNASSIGNED;
+	head->next = p;
+	p->next = NULL;
+
+	int n = 0;
+	while (1)
+	{
+		Show(head);
+		cout << "输入：\n\t" << "1\t申请空间" << "\n\t2\t释放空间"<<"\n\t其他\t退出" << endl;
+		cin >> n;
+		switch (n)
+		{
+		case 1:
+			NewTask(head);
+			break;
+		case 2:
+			DeleteTask(head);
+			break; 
+		default:
+			delete p, head;
+			exit(0);
+			break;
+		}
+	}
+	return 0;
+}
---
+++ OsExp/Project3/test.cpp
@@ -0,0 +1,160 @@
+﻿/*
+用位示图管理磁盘存储空间
+[提示]：
+(1) 为了提高磁盘存储空间的利用率，可在磁盘上组织成链接文件、索引文件，
+这类文件可以把逻辑记录存放在不连续的存储空间。
+为了表示哪些磁盘空间已被占用，哪些磁盘空间是空闲的，可用位示图来指出。
+位示图由若干字节构成，每一位与磁盘上的一块对应，“1”状态表示相应块已占用，“0”状态表示该块为空闲。
+位示图的形式与实习二中的位示图一样，但要注意，对于主存储空间和磁盘存储空间应该用不同的位示图来管理，绝不可混用。
+(2) 申请一块磁盘空间时，由分配程序查位示图，找出一个为“0”的位，计算出这一位对应块的磁盘物理地址，且把该位置成占用状态“1”。
+
+假设现在有一个盘组共8个柱面，每个柱面有2个磁道（盘面），每个磁道分成4个物理记录。
+
+那么，当在位示图中找到某一字节的某一位为“0”时，这个空闲块对应的磁盘物理地址为：
+柱面号=字节号
+磁道号= 位数 / 4
+物理记录号= 位数 % 4
+(3) 归还一块磁盘空间时，由回收程序根据归还的磁盘物理地址计算出归还块在位示图中的对应位，把该位置成“0”。
+按照（2）中假设的盘组，归还块在位示图中的位置计算如下：
+字节号=柱面号
+位数=磁道号 *4+物理记录号
+(4) 设计申请磁盘空间和归还磁盘空间的程序。
+要求能接受来自键盘的空间申请及释放请求，要求能显示或打印程序运行前和运行后的位示图；
+分配时把分配到的磁盘空间的物理地址显示或打印出来，归还时把归还块对应于位示图的字节号和位数显示或打印出来。
+*/
+
+#include <iostream>
+using namespace std;
+
+#define CYLINDER 8
+#define TRACK 2
+#define RECORD 4
+#define FREE 0
+#define OCCUPIED 1
+
+int bitMap[CYLINDER][CYLINDER];
+
+void Assign()
+{
+	int need = 0;
+	int rest = 0;
+	cout << "请输入需要分配的空间大小：" << endl;
+
+	cin >> need;
+	if (need <= 0)
+	{
+		cout << "请按要求输入数据！" << endl;
+		exit(0);
+	}
+
+	for (int i = 0; i < CYLINDER; i++)
+	{
+		for (int j = 0; j < CYLINDER; j++)
+		{
+			if (bitMap[i][j] == FREE)
+			{
+				rest++;
+			}
+		}
+	}
+
+	if (need <= rest)
+	{
+		cout << "分配成功！" << endl;
+		cout << "柱面\t磁道\t物理记录" << endl;
+		for (int i = 0; i < CYLINDER; i++)
+		{
+			for (int j = 0; j < CYLINDER; j++)
+			{
+				if (bitMap[i][j] == FREE)
+				{
+					if (need <= 0)
+					{
+						break;
+					}
+
+					bitMap[i][j] = OCCUPIED;
+
+					cout << i << "\t" << j / RECORD << "\t" << j % RECORD << endl;
+
+					need--;
+				}
+			}
+		}
+	}
+	else
+	{
+		cout << "分配失败!" << endl;
+	}
+
+}
+
+void Return()
+{
+	int cylinder, track, record;
+	int byte, bit;
+
+	cout << "请依次输入要回收的柱面号(0 - 7)、磁道号(0 - 2)、物理块号(0 - 3)：" << endl;
+
+	cin >> cylinder >> track >> record;
+	if (cylinder < 0 || cylinder>7 || track < 0 || track>2 || record < 0 || record>3)
+	{
+		cout << "请按要求输入数据！" << endl;
+		exit(0);
+	}
+
+	byte = cylinder;
+	bit = track * RECORD + record;
+
+	bitMap[byte][bit] = FREE;
+
+	cout << "归还成功！" << endl;
+	cout << "字节号\t" << "位数\t" << endl;
+	cout << byte << "\t" << bit << endl;
+}
+
+void Show()
+{
+	cout << "********************************位视图********************************" << endl;
+	for (int i = 0; i < CYLINDER; i++)
+	{
+		cout << "\t" << i;
+	}
+	cout << "\n********************************************************************" << endl;
+
+	for (int i = 0; i < CYLINDER; i++)
+	{
+		cout << i << "  *"<<"\t";
+		for (int j = 0; j < CYLINDER; j++)
+		{
+			cout << bitMap[i][j] << "\t";
+		}
+		cout << endl;
+	}
+	cout<< "********************************************************************" << endl;
+}
+
+int main()
+{
+	int n;
+	while (1)
+	{
+		Show();
+		cout << "输入：\n\t" << "1\t分配" << "\n\t2\t回收" << "\n\t其他\t退出" << endl;
+		cin >> n;
+		switch (n)
+		{
+		case 1:
+			Assign();
+			break;
+		case 2:
+			Return();
+			break;
+		default:
+			exit(0);
+			break;
+		}
+	}
+
+	return 0;
+}
